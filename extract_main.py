2427a2ed39708c889f6b444fde55466f837bcfaf
EzekTec <stephen.ezekwem@ezektec.com>
2026-03-17 15:47:53 -0600

    chore: clean up unused test scripts and patch files

--- extract_main.py
+++
@@ -1,6 +0,0 @@
-import re
-
-with open("src/main.rs", "r") as f:
-    content = f.read()
-
-# I will write a manual extraction in Rust.
--- test_ct_eq.rs
+++
@@ -1,6 +0,0 @@
-use subtle::ConstantTimeEq;
-fn main() {
-    let a = b"123";
-    let b = b"1234";
-    let _ = a.ct_eq(b);
-}
