d325aacbe2162f3296d70210ebf3d89614fe5e15
EzekTec <stephen.ezekwem@ezektec.com>
2026-05-18 12:10:35 -0600

    cade-cp-auto

--- test_lua.rs
+++ test_lua.rs
@@ -1,9 +1,2 @@
-fn main() {
-    let engine = cade_tui::lua_engine::LuaEngine::new().unwrap();
-    let cwd = std::env::current_dir().unwrap();
-    let plugin_dir = cwd.join(".cade").join("plugins");
-    println!("Loading plugins from {:?}", plugin_dir);
-    engine.load_plugins(&plugin_dir);
-    let header = engine.get_header_ui();
-    println!("Header: {:?}", header);
-}
+use mlua::Lua;
+fn main() {}
